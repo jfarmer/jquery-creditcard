7277c0087ecb6fa9ddcdaceeb1e95e77b7ee58c6
Jesse Farmer <jesse@everlane.com>
2011-08-24 17:59:47 -0700

    Use both input keyup

--- jquery.creditcard-0.1.js
+++ jquery.creditcard-0.1.js
@@ -1,18 +1,19 @@
 (function( $ ){
-
-  $.fn.creditCard = function( options ) {  
-
-   var cardTypes = [{ name: "Visa", lengths: [13,16], pattern: /^4/},
+  $.fn.creditCard = function( options ) {
+    var cardTypes = [{ name: "Visa", lengths: [13,16], pattern: /^4/},
                     { name: "MasterCard", lengths: [16], pattern: /^5[1-5]/},
                     { name: "DinersClub", lengths: [14,16], pattern: /^3[0,6,8]/},
                     { name: "AmEx", lengths: [15], pattern: /^3[4,7]/},
                     { name: "Discover", lengths: [16], pattern: /^6011/},
                     { name: "JCB", lengths: [16], pattern: /^35/}]
-    
-    
+
+    var classNames = cardTypes.map(function(cardType, idx) {
+      return cardType.name.toLowerCase();
+    }).join(' ');
+
     var methods = {
       getCardType: function(cardNumber) {
-        cardNumber = cardNumber.replace(/\D/g, ""); 
+        cardNumber = cardNumber.replace(/\D/g, "");
         for(var i = 0;i < cardTypes.length;i++) {
           if (cardTypes[i].pattern.test(cardNumber)) {
             return cardTypes[i].name;
@@ -21,14 +22,18 @@
       }
     }
 
-    return this.each(function() {        
-      if ( options ) { 
+    var settings = {};
+
+    return this.each(function() {
+      if ( options ) {
         $.extend( settings, options );
       }
 
-      $(this).bind('keypress', function(e) {
+      $(this).bind('input keyup', function(e) {
         if (cardType = methods.getCardType($(this).val())) {
-          $(this).addClass(cardType.toLowerCase());
+          $(this).removeClass(classNames).addClass(cardType.toLowerCase());
+        } else {
+          $(this).removeClass(classNames);
         }
       });
     });
