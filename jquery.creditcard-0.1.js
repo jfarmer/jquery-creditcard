ee7bad3a6eff5db6cb3d67afd623576e1f8d3634
Nan Yu <nan@everlane.com>
2011-12-07 17:55:39 -0800

    added callback to the credit card trigger

--- jquery.creditcard-0.1.js
+++ jquery.creditcard-0.1.js
@@ -1,19 +1,19 @@
 (function( $ ){
-  $.fn.creditCard = function( options ) {
+  $.fn.creditCard = function( options ) {  
     var cardTypes = [{ name: "Visa", lengths: [13,16], pattern: /^4/},
                     { name: "MasterCard", lengths: [16], pattern: /^5[1-5]/},
                     { name: "DinersClub", lengths: [14,16], pattern: /^3[0,6,8]/},
                     { name: "AmEx", lengths: [15], pattern: /^3[4,7]/},
                     { name: "Discover", lengths: [16], pattern: /^6011/},
                     { name: "JCB", lengths: [16], pattern: /^35/}]
-
+    
     var classNames = cardTypes.map(function(cardType, idx) {
       return cardType.name.toLowerCase();
     }).join(' ');
-
+        
     var methods = {
       getCardType: function(cardNumber) {
-        cardNumber = cardNumber.replace(/\D/g, "");
+        cardNumber = cardNumber.replace(/\D/g, ""); 
         for(var i = 0;i < cardTypes.length;i++) {
           if (cardTypes[i].pattern.test(cardNumber)) {
             return cardTypes[i].name;
@@ -21,19 +21,25 @@
         }
       }
     }
-
+    
     var settings = {};
 
-    return this.each(function() {
-      if ( options ) {
+    return this.each(function() {        
+      if ( options ) { 
         $.extend( settings, options );
       }
-
+      
       $(this).bind('input keyup', function(e) {
+				var cardType;
         if (cardType = methods.getCardType($(this).val())) {
+					
           $(this).removeClass(classNames).addClass(cardType.toLowerCase());
+					if(settings.fn)
+						settings.fn(cardType.toLowerCase());
         } else {
           $(this).removeClass(classNames);
+					if(settings.fn)
+						settings.fn(null);
         }
       });
     });
